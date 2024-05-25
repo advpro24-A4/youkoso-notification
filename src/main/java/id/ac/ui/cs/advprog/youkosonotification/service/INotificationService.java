1008038b02944e46121ed561bc5b19e1f421645d
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-05-25 21:01:16 +0700

    [GREEN] Implement notification service

---
+++ src/main/java/id/ac/ui/cs/advprog/youkosonotification/service/INotificationService.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.youkosonotification.service;
+
+import id.ac.ui.cs.advprog.youkosonotification.model.Notification;
+
+public interface INotificationService {
+    void createNotification(Notification notification);
+}
